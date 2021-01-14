1aea93e903dd1a6feddfb2a3be3cf37ff37f437e
Mario <marketingkg@mail.ru>
2021-01-14 17:51:52 +0600

    popupdone

--- Components/modal/modal.js
+++ Components/modal/modal.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {useAppContext }from '../context'
 
-export default function Modal() {
+export default function Modal({showModal}) {
+    const {notShowTheModal} = useAppContext()
     return (
-        <div>
-            
+        <div className={showModal?'show-modal':'not-Show'}>
+            <div className="close"> <button  onClick={()=>notShowTheModal()}>mario</button></div>
+            <div className="intermodal">this is the information</div>
         </div>
     )
 }
