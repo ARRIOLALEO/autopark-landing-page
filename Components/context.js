8d1b0362f127ee75527923af487006be12d83251
Mario <marketingkg@mail.ru>
2021-01-16 12:08:49 +0600

    addMenuLandingpage

--- Components/context.js
+++ Components/context.js
@@ -4,13 +4,20 @@ const AppContext = createContext()
 
 export function AppWraper({ children}){
     const [showModal, setShowModal] = useState(false)
+    const [showMenu ,setShowMenu] =useState(false)
     function showTheModal(){
         setShowModal(true)
     }
     const notShowTheModal = () =>{
         setShowModal(false)
     }
-    let shareState ={allCars,showModal,showTheModal,notShowTheModal}
+    const showMenuOn =() =>{
+        setShowMenu(true)
+    }
+    const notShowMenu = () =>{
+        setShowMenu(false)
+    }
+    let shareState ={allCars,showModal,showTheModal,notShowTheModal ,showMenu ,showMenuOn, notShowMenu}
     return (<AppContext.Provider value={shareState}>
         {children}
     </AppContext.Provider>
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,30 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Hero from '../Components/hero/Hero'
 import Cars from '../Components/Cars/Cars'
+import {useAppContext} from '../Components/context'
+import Menu  from "../Components/menu"
 
 
 export default function Home() {
+  const {showMenu , showMenuOn } = useAppContext()
   return (
     <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Menu showTheMenu = {showMenu}/>
 <nav className="nav">
   <div className="nav-center">
     <div className="nav-header">
       <img src="/images/autopark.png" alt="" className="nav-logo"/>
-      <button className="btn toggle-btn"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg></button>
+      <button className="btn toggle-btn" onClick={()=> showMenuOn()}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg></button>
+      
     </div>
     
   </div>
 </nav>
+
  <Hero/>
  <Cars/>
 
