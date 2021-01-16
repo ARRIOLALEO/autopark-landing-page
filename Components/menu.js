f0dd9982267ed909fa5e0b4a2ecf3618b21022a4
Mario <marketingkg@mail.ru>
2021-01-16 12:39:18 +0600

    translations

--- Components/menu.js
+++ Components/menu.js
@@ -9,19 +9,18 @@ export default function Menu({showTheMenu}) {
     const {notShowMenu } = useAppContext()
     return (
         <div className={showTheMenu?'show-menu center-elements':'notshow-menu'}>
-            <button onClick={()=>notShowMenu()}>Close the menu</button>
-            <h1> Contac Us </h1>
+            <button className="btn" onClick={()=>notShowMenu()}>Close the menu</button>
                 <div className="contact-elements-1">
-           <a href="https://wa.me/996555858585"> <FaWhatsapp size={50} color="green"/> </a>
+           <a href="https://wa.me/996555858585"> <FaWhatsapp size={40} color="green"/><span class="text">+996 555858585</span> </a>
         </div>
         <div className="contact-elements-2">
-            <a href=""><FcIphone size={50}/></a>
+            <a href=""><FcIphone size={40}/><span class="text">+996 555858585</span></a>
         </div>
         <div className="contact-elements-3">
-            <a  href="https://www.instagram.com/autopark.kgz/" target="_blank"><GrInstagram size={50}  color="blue"/></a>
+            <a  href="https://www.instagram.com/autopark.kgz/" target="_blank"><GrInstagram size={40}  color="blue"/><span class="text">@autopark.kgz</span> </a>
         </div>
          <div className="contact-elements-4">
-            <a href="https://www.facebook.com/CarRentKyrgyzstan" target="_blank"><AiFillFacebook size={50} color="blue"/></a>
+            <a href="https://www.facebook.com/CarRentKyrgyzstan" target="_blank"><AiFillFacebook size={40} color="blue"/><span class="text">@CarRentKyrgyzstan</span>  </a>
          </div>
         </div>
     )
